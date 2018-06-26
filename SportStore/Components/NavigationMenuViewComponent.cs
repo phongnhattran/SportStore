4d443f856d2023c15daa0f26ec1f3dae548cbf8d
phong <phongnhattran@gmail.com>
2018-06-26 10:45:40 +0700

    First Clone

---
+++ SportStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportStore.Models;
+
+namespace SportStore.Components
+{
+    public class NavigationMenuViewComponent: ViewComponent
+    {
+        IProductRepository productRepository;
+
+        public NavigationMenuViewComponent(IProductRepository repository)
+        {
+            productRepository = repository;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(productRepository.Products.Select(p => p.Category).Distinct().OrderBy(p => p));
+        }
+    }
+}
---
+++ SportStore/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportStore.Models;
+using SportStore.Models.ViewModels;
+
+namespace SportStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        IProductRepository repository;
+        public int pageSize = 4;
+        public ProductController(IProductRepository productRepository)
+        {
+            repository = productRepository;
+        }
+        public ViewResult List(string category, int productPage = 1) => View(new ProductsListViewModel
+        {
+            Products = repository.Products.Where(p => p.Category == category || category == null)
+                                            .OrderBy(p => p.ProductID)
+                                            .Skip((productPage - 1) * pageSize)
+                                            .Take(pageSize),
+            PagingInfo = new PagingInfo { CurrentPage = productPage, ItemsPerPage = pageSize,
+                                          TotalItems = category == null ? repository.Products.Count() : repository.Products.Where(p => p.Category == category).Count() },
+            CurrentCategory = category
+        });
+    }
+}
