02023ffece419f13625d772295f4570f5f4a955e
phong <phongnhattran@gmail.com>
2018-07-17 15:47:02 +0700

    Add configuration project

---
+++ ConfiguringApps/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace ConfiguringApps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return new WebHostBuilder().UseKestrel().UseContentRoot(Directory.GetCurrentDirectory()).
+                    UseIISIntegration().UseStartup<Startup>().Build();
+
+                        
+        }
+    }
+}
