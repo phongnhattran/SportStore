623f59b74bd1f9107df5faf71cb8e7351d32f6a5
PTLAP\Phong Phonix <phongnhattran@gmail.com>
2018-08-13 22:25:52 +0700

    Filters

---
+++ Filters/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Filters.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Filters.Controllers
+{
+    [RequireHttps]
+    [TypeFilter(typeof(DiagnosticsFilter))]
+    [TypeFilter(typeof(TimeFilter))]
+    public class HomeController : Controller
+    {
+        //[RequireHttps]
+        public ViewResult Index() => View("Message", "This is the Index action on the Home controller");
+
+        //[RequireHttps]
+        public ViewResult SecondAction() => View("Message", "This is the Index action on the Home controller");
+
+        public ViewResult GenerateException(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            else if (id > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            else
+            {
+                return View("Message", $"The value is {id}");
+            }
+        }
+        //public IActionResult Index()
+        //{
+        //    if (!Request.IsHttps)
+        //    {
+        //        return new StatusCodeResult(StatusCodes.Status403Forbidden);
+        //    }
+        //    else
+        //    {
+        //        return View("Message",
+        //        "This is the Index action on the Home controller");
+        //    }
+        //}
+
+        //public IActionResult SecondAction()
+        //{
+        //    if (!Request.IsHttps)
+        //    {
+        //        return new StatusCodeResult(StatusCodes.Status403Forbidden);
+        //    }
+        //    else
+        //    {
+        //        return View("Message",
+        //        "This is the SecondAction action on the Home controller");
+        //    }
+        //}
+    }
+}
---
+++ Filters/Infrastructure/TimeFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filters.Infrastructure
+{
+    public class TimeFilter : IAsyncActionFilter, IAsyncResultFilter
+    {
+        private Stopwatch timer;
+        private IFilterDiagnostics diagnostics;
+        public TimeFilter(IFilterDiagnostics diags)
+        {
+            diagnostics = diags;
+        }
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            timer = Stopwatch.StartNew();
+            await next();
+            diagnostics.AddMessage($@"Action time: {timer.Elapsed.TotalMilliseconds}");
+        }
+        public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            await next();
+            timer.Stop();
+            diagnostics.AddMessage($@"Result time: {timer.Elapsed.TotalMilliseconds}");
+        }
+    }
+}
